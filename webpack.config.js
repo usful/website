b00e59b329d60487a46c24c1cc12f3c0e20e40e2
Clinton Robinson <clinton.robinson@gmail.com>
2017-07-02 09:53:42 -0400

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,3 @@
+module.exports = function(env) {
+  return require(`./webpack.${env || 'dev'}.js`)
+}
---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: ['whatwg-fetch', 'babel-polyfill', path.resolve(__dirname, 'src/main.js')],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'index_bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css', '.scss'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules\/(?!lane-shared)/,
+        loader: 'babel-loader',
+        query: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015', 'stage-0', 'react'],
+        },
+      },
+      {
+        // scss styles are loaded with modules local scope
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader?modules&localIdentName=' +
+        '[local]---[hash:base64:5]!sass-loader!postcss-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEV__: true,
+    }),
+  ],
+  devtool: 'source-map',
+  watch: true,
+};
