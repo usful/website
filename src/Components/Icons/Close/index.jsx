2d0439ad398f8bfcdd1b7fa29efdb33d4588c8fc
Clinton Robinson <clinton.robinson@gmail.com>
2017-07-12 15:55:28 -0400

    Adding icons

---
+++ src/Components/Icons/Close/index.jsx
@@ -0,0 +1,14 @@
+
+<svg width="37px" height="35px" viewBox="0 0 37 35" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <!-- Generator: Sketch 45 (43475) - http://www.bohemiancoding.com/sketch -->
+  <desc>Created with Sketch.</desc>
+  <defs></defs>
+  <g id="experiences" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="square">
+    <g id="experience-project-desktop" transform="translate(-1385.000000, -19.000000)" stroke="#FFFFFF" stroke-width="0.8">
+      <g id="close-button" transform="translate(1386.000000, 19.000000)">
+        <path d="M0.397727273,34.6022727 L34.7407345,0.259265451" id="Line"></path>
+        <path d="M0.397727273,1.19318182 L33.6977386,34.4931931" id="Line"></path>
+      </g>
+    </g>
+  </g>
+</svg>
