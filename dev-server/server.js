1cb7fd876bedd1f52c27cbbeec8d3863fa11b2d6
Tyler Mizuyabu <tyler@monanetworks.com>
2017-09-27 12:26:48 -0400

    removed https from dev server

--- dev-server/server.js
+++ dev-server/server.js
@@ -6,8 +6,8 @@ const webpack = require('webpack');
 const path = require('path');
 const compression = require('compression');
 const http = require('http');
-const https = require('https');
-const fs = require('fs');
+// const https = require('https');
+// const fs = require('fs');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const aws = require('aws-sdk');
@@ -17,27 +17,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(compression());
 
-app.use((req, res, next) => {
+/** app.use((req, res, next) => {
   if (!req.secure) {
     res.writeHead(301, {
-      Location: `https://${req.headers['host']}${req.url}`
+      Location: `https://${req.headers.host}${req.url}`,
     });
 
     res.end();
   }
 
   next();
-});
+}); */
 
 // put in the actual config here.
 aws.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
-  region: process.env.AWS_REGION
+  region: process.env.AWS_REGION,
 });
 
 const transporter = nodemailer.createTransport({
-  SES: new aws.SES({ apiVersion: '2010-12-01' })
+  SES: new aws.SES({ apiVersion: '2010-12-01' }),
 });
 
 app.post('/api/contact-us', async (req, res) => {
@@ -48,7 +48,7 @@ app.post('/api/contact-us', async (req, res) => {
     text: `
 From: ${req.body.email},
 Subject: ${req.body.subject},
-Message: ${req.body.message}`
+Message: ${req.body.message}`,
   };
 
   const response = await new Promise((resolve, reject) => {
@@ -73,15 +73,15 @@ app.use(
     publicPath: '/', // Same as `output.publicPath` in most cases.
     watchOptions: {
       aggregateTimeout: 300,
-      poll: true
-    }
-  })
+      poll: true,
+    },
+  }),
 );
 
 const sendIndex = (req, res) =>
-  res.sendFile(path.join(__dirname + '/../public/index.html'));
+  res.sendFile(path.join(`${__dirname}/../public/index.html`));
 
-['/technology', '/experiences', '/market'].forEach(url => {
+['/technology', '/experiences', '/market'].forEach((url) => {
   app.use(url, sendIndex);
   app.use(`${path}/*`, sendIndex);
 });
@@ -91,11 +91,11 @@ app.use('/', express.static('public'));
 http.createServer(app).listen(8080);
 console.log('http listening on port 8080!');
 
-const options = {
+/** const options = {
   key: fs.readFileSync(path.join(__dirname, '../prod-server/ssl/key.pem')),
   cert: fs.readFileSync(path.join(__dirname, '../prod-server/ssl/cert.pem')),
   ca: fs.readFileSync(path.join(__dirname, '../prod-server/ssl/fullchain.pem'))
 };
 
 https.createServer(options, app).listen(9090);
-console.log('https listening on port 9090!');
+console.log('https listening on port 9090!'); */
