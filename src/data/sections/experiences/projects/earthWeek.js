0154509282f5711c9613c34ef5938b942492d009
Tyler Mizuyabu <tyler@monanetworks.com>
2017-10-03 09:22:09 -0400

    updated earth-week layout and text blocks. Still need some assets

--- src/data/sections/experiences/projects/earthWeek.js
+++ src/data/sections/experiences/projects/earthWeek.js
@@ -19,13 +19,13 @@ module.exports = {
     'Over 20,000 people engaged with the installation further recognizing our clients commitment to sustainability & increased awareness for all partners.',
   content: [
     {
-      id: 'b18',
+      id: 'earth-week-1',
       type: 'TextBlock',
       text:
         'Our team was responsible for the design, concept, and execution of the Earth Week installation. This began with extensive research and development, as we sourced the latest and greatest growing technology.'
     },
     {
-      id: 'b2',
+      id: 'earth-week-2',
       type: 'ImageBlock',
       url:
         'https://s3.amazonaws.com/usful-portfolio/img/experiences/earth-week-5.jpg',
@@ -33,30 +33,32 @@ module.exports = {
       text: 'Designed to introduce the concept of urban farming in a palatable and hands-on manner, the installation showcased the simplicity of urban farming by highlighting growing technologies.',
     },
     {
-      id: 'b19',
-      type: 'TextBlock',
-      text:
-        'Through the design and construction of the installation, people experienced this technology for themselves, witnessing firsthand how urban gardening could become a reality. '
-    },
-    {
-      id: 'b3',
+      id: 'earth-week-3',
       type: 'ImageBlock',
-      url:
-        'https://s3.amazonaws.com/usful-portfolio/img/experiences/earth-week-2.jpg',
-      align: 'full'
+      align: 'right',
+      text: 'Microgreen growing units',
+      url: 'https://s3.amazonaws.com/usful-portfolio/img/experiences/earth-week-6.jpg',
     },
     {
-      id: 'b20',
-      type: 'TextBlock',
-      text:
-        'In addition to attracting curious passersby, we invited a curated selection of guests for a series of informative lunch events in the space, inspiring a powerful dialogue on the future of food.'
-    },
-    {
-      id: 'b4',
+      id: 'earth-week-4',
       type: 'ImageBlock',
-      url:
-        'https://s3.amazonaws.com/usful-portfolio/img/experiences/earth-week-3.jpg',
-      align: 'full'
-    }
+      align: 'left',
+      text: 'Language of Plants',
+      url: 'https://s3.amazonaws.com/usful-portfolio/img/experiences/earth-week-7.jpg',
+    },
+    {
+      id: 'earth-week-5',
+      type: 'ImageBlock',
+      align: 'right',
+      text: '',
+      url: 'https://s3.amazonaws.com/usful-portfolio/img/experiences/earth-week-2.jpg',
+    },
+    {
+      id: 'earth-week-6',
+      type: 'ImageBlock',
+      align: 'center',
+      text: 'Through the design and construction of the installation, people experienced this technology for themselves, witnessing firsthand how urban gardening could become a reality. In addition to attracting curious passersby, we invited a curated selection of guests for a series of informative lunch events in the space, inspiring a powerful dialogue on the future of food.',
+      url: 'https://s3.amazonaws.com/usful-portfolio/img/experiences/earth-week-1.jpg',
+    },
   ] //Blocks
 };
