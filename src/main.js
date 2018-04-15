840a410639b4b4aeafa5c0d90e453ac7bc427d64
Clinton Robinson <clinton.robinson@gmail.com>
2018-04-15 10:36:34 -0400

    WIP

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import 'core-js/es6/set';
 
 const TRACKING_ID = 'UA-57226902-1';
 
-if (typeof gtag === 'undefined') {
-  window.analytics = gtag;
-} else {
+if (typeof(window.gtag) === 'undefined') {
   window.analytics = () => {};
+} else {
+  window.analytics = gtag;
 }
 
 window.analytics('js', new Date());
